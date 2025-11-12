2354bb1a84be8c79d633332609047ab52c4de6d1
AlisonCumbajin <cumbajinalison2001@gmail.com>
2025-11-12 12:48:25 -0500

    backend de titulación

---
+++ src/main/java/ec/yavirac/yavigestion/YavigestionApplication.java
@@ -0,0 +1,13 @@
+package ec.yavirac.yavigestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YavigestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YavigestionApplication.class, args);
+	}
+
+}
